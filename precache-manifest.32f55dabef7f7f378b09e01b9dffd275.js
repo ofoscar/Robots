2c6c9660c8cdc8964cdbc6c254346274930a4a50
Oscar R <ktaoscar@gmail.com>
2020-07-03 21:04:22 -0600

    Updates

---
+++ precache-manifest.32f55dabef7f7f378b09e01b9dffd275.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ad6533679dd81698adf388a6e08f7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ee8420e30e3f48060f52",
+    "url": "/static/css/main.296014eb.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8420e30e3f48060f52",
+    "url": "/static/js/main.ed549214.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
